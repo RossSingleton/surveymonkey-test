e9f0ff01f822a8fe5eeec0e10d08bc1f4a9ba024
RossSingleton <ross-singleton@hotmail.co.uk>
2019-04-04 10:36:21 +0100

    Split functionality into two files, one for getting survey, one for getting people who failed test

--- getFailures.py
+++ getFailures.py
@@ -11,7 +11,7 @@ headers = {
 data = {}
 
 HOST = "https://api.surveymonkey.net"
-SURVEY_LIST_ENDPOINT = "/v3/surveys"
+SURVEY_LIST_ENDPOINT = "/v3/surveys/171070012/responses/bulk"
 
 uri = "%s%s" % (HOST, SURVEY_LIST_ENDPOINT)
 
@@ -20,4 +20,10 @@ response = client.get(uri, headers=headers)
 response_json = response.json()
 # survey_list = response_json["data"]["surveys"]
 
-print(response_json)
+print(response_json['data'])
+total = response_json['total']
+# failure_array = []
+
+for i in range(0, total):
+    if response_json['data'][i]['quiz_results']['incorrect'] > 0:
+        print(response_json['data'][i]['metadata']['contact']['email']['value'])
